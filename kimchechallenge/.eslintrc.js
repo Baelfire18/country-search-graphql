fa10bdaceda30845cc32f9f98fae3a6842d61bb3
Baelfire18 <jacastro18@uc.cl>
2022-01-21 17:19:16 -0300

    refactor(linter): update linter rules & correct indentations

--- kimchechallenge/.eslintrc.js
+++ kimchechallenge/.eslintrc.js
@@ -1,22 +1,33 @@
 module.exports = {
-    env: {
-      browser: true,
-      es2021: true
+  env: {
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    extends: [
-      'plugin:react/recommended',
-      'airbnb',
-    ],
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'func-names': 'off',
+    'react/prop-types': 'off',
+    'react/function-component-definition': 'off',
+    'import/extensions': [
+      'error',
+      'ignorePackages',
+      {
+        js: 'never',
+        jsx: 'never',
       },
-      ecmaVersion: 12,
-      sourceType: 'module',
-    },
-    plugins: [
-      'react',
     ],
-    rules: {
-    },
-  };
+  },
+};
